3587059c34dea83f68aa0bb937e795f9b44517ac
SergioHMagalhaes <ssergioh3@gmail.com>
2025-05-13 15:07:10 -0300

    feat: adiciona endpoint pra salvar despesa

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,11 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterdExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        return new ResponseRegisterdExpenseJson();
+    }
+}
