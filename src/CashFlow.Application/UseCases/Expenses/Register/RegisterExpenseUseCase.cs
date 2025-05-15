6bbd7f939ded6795172d7edc4442bcd147079e41
SergioHMagalhaes <ssergioh3@gmail.com>
2025-05-15 14:24:24 -0300

    feat: adiciona validação de erros

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,11 +1,26 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase
 {
     public ResponseRegisterdExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
         return new ResponseRegisterdExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
+       
+        if (result.IsValid == false)
+        {
+            var errorMessage = result.Errors.Select(f => f.ErrorMessage).ToList();
+            throw new ErrorOnValidationException(errorMessage);
+        }
     }
 }
